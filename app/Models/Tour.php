b20bf9ab78db51bdfcb62178338a5d179ce5a501
Odame <kwesi.danquah@daaseng.com>
2023-06-20 23:17:19 +0100

    setup model to use UUID

---
+++ app/Models/Tour.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Tour extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $table = 'tours';
+
+    protected $guarded = ['id'];
+
+    public function price(): Attribute{
+        return Attribute::make(
+            get: fn($value) => $value / 100,
+            set: fn ($value) => $value * 100
+        );
+    }
+}
--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -2,20 +2,42 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Travel extends Model
 {
-    use HasFactory;
+    use HasFactory,Sluggable, HasUuids;
 
     protected $table = 'travels';
 
     protected $guarded = ['id'];
 
-    //observe  travel model
-    protected static function booted()
+    public function tours(): HasMany{
+        return $this->hasMany(Tour::class);
+    }
+     /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
     {
-        self::observe(Travel::class);
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
+    // virtual column // accessor
+    public function numberOfNights(): Attribute{
+        return Attribute::make(
+            get: fn($value,$attributes) => $attributes['number_of_days'] - 1
+        );
     }
 }
