407fc7a7ecf2f449378f22ba3a855531571ae72a
Odame <kwesi.danquah@daaseng.com>
2023-06-22 00:07:23 +0100

    setup store travel method

--- app/Http/Controllers/Api/v1/TravelController.php
+++ app/Http/Controllers/Api/v1/TravelController.php
@@ -2,28 +2,31 @@
 
 namespace App\Http\Controllers\Api\v1;
 
+use App\Http\Controllers\Controller;
+use App\Http\Requests\TravelRequest;
+use App\Http\Resources\TravelResource;
 use App\Models\Travel;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use App\Http\Resources\TravelResource;
 
 class TravelController extends Controller
 {
-
     public function index()
     {
         //
-        $travels = Travel::where('is_public',true)->paginate(10);
-        
+        $travels = Travel::where('is_public', true)->paginate(10);
+
         return TravelResource::collection($travels);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(TravelRequest $request)
     {
         //
+        $data = $request->validated();
+
+        return new TravelResource(Travel::create($data));
     }
 
     /**
