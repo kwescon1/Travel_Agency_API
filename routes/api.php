cf035d1324f1b6a082527e43cf4c0af672da3a1e
Odame <kwesi.danquah@daaseng.com>
2023-06-21 02:43:51 +0100

    define travels api route

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('travels',\App\Http\Controllers\Api\v1\TravelController::class)->only('index');
