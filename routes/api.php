a64d8dc38183d9a140117f531e681c1cd1bb4846
Odame <kwesi.danquah@daaseng.com>
2023-06-22 01:40:19 +0100

    remove unwanted route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +12,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::apiResource('travels', \App\Http\Controllers\Api\v1\TravelController::class)->only('index');
 
