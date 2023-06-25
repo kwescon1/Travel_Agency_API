199eaf5fa466806ccb79c16f65fff46be4ad3559
Odame <kwesi.danquah@daaseng.com>
2023-06-26 00:50:41 +0100

    remove duplicate functionality

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,11 +46,6 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
-    protected static function booted()
-    {
-        self::observe(User::class);
-    }
-
     public function roles(): BelongsToMany
     {
         return $this->belongsToMany(Role::class);
